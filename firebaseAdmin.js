8b47c5021c90b3685427399f92b700347bcb2986
fundacionmisionvida7 <fundacionmisionvida7@gmail.com>
2025-04-15 21:21:47 -0300

    Update firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,12 +1,17 @@
 // firebaseAdmin.js
 import admin from 'firebase-admin';
 
+console.log("FIREBASE_SERVICE_ACCOUNT:", process.env.FIREBASE_SERVICE_ACCOUNT); // Agrega este log temporalmente
+
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
+  console.log("Firebase Admin inicializado correctamente");
+} else {
+  console.log("Firebase Admin ya estaba inicializado");
 }
 
 export default admin;
